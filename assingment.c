012ae72945bd97bbb17c74c38329e269170c1bf9
Rishav Bhardwaz <rishav76000@gmail.com>
2022-05-04 08:12:38 +0530

    easy way

---
+++ assingment.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+int x;
+    /* not taking a variable because using only printf cmd to execute the code
+    this table is representation of bhagalpur railway station time table on 29/4/2022*/
+    printf ("Welcome and Greeting's for the day from Bhagalpur Railway team \n") ;
+    printf("Time table of Bhagalpur junction for today \n ");
+   //Kepping in mind this code was written on 29/4 I have used the station data for the same particular day  
+
+        printf("Time table for Bhagalpur Station on 29/4\n");
+        printf(" S.No          Train Name          Arrival          Departure          Source          Destination\n");
+        printf(" 1            Bgp Has Demu          ----                  7:05             Bhagalpur      Hansdila \n");
+        printf(" 2            Howrah Super Exp    08:56                9:06             Jamalpur        Howrah \n");
+        printf(" 3            Jamalpur Exp            21:25                23:05           Bhagalpur       Jamalpur\n");
+        printf(" 4            Bhagalpur Intercity   22:15                -----            Danapur          Bhagalpur\n");
+        printf(" 5            Farakka Exp               23:27                23:37          Tinpahar          Delhi\n");
+        printf(" 6             Patna Exp                  23:53                23:58          Farakka            Patna\n");
+  
+    
+printf("Thanks for running the code\n");
+printf("Written by Rishav Bhardwaz\n Roll no- 32\n Semester 1\n");        
+  
+return 0;
+}
